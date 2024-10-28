bd242ef9e09c0284a9e2a0ee2686aec76cbe3784
soldier <dboumertit@edenschool.fr>
2024-10-28 16:37:11 +0100

    feat(app): exo js fini

---
+++ main.js
@@ -0,0 +1,141 @@
+// function sumTwoNumbers(a, b) {
+//     console.log(a + b);
+    
+// }
+
+// sumTwoNumbers(3, 7);
+// sumTwoNumbers(-1, 5);
+// sumTwoNumbers(0, 0);
+// sumTwoNumbers(10, -10);
+// sumTwoNumbers(1.5, 2.5);
+
+// function concatenateStrings(str1, str2) {
+//     console.log(str1.concat(str2));
+    
+// }
+
+// concatenateStrings("Hello", "World");
+// concatenateStrings("foo", "bar");
+// concatenateStrings("", "test");
+// concatenateStrings("123", "456");
+// concatenateStrings("JavaScript", "Rocks");
+
+// function isEven(num) {
+//     if(num % 2 === 0) {
+//         console.log(true);
+//     }else {
+//         console.log(false);
+        
+//     }
+// }
+
+// isEven(4);
+// isEven(7);
+// isEven(0);
+// isEven(-2);
+// isEven(101);
+
+// function rectangleArea(width, height) {
+//     console.log(width * height);
+// }
+
+// rectangleArea(4, 5);
+// rectangleArea(10, 3);
+// rectangleArea(6.5, 2.5);
+// rectangleArea(0, 10);
+// rectangleArea(5, 5);
+
+// function isAdult(num) {
+//     if(num >= 18) {
+//         console.log(true);
+        
+//     }else {
+//         console.log(false);
+        
+//     }
+// }
+
+// isAdult(20);
+// isAdult(17);
+// isAdult(18);
+// isAdult(30);
+// isAdult(5);
+
+// function isPositive(num) {
+//     if(num > 0) {
+//         console.log(true);
+        
+//     }else {
+//         console.log(false);
+//     }
+// }
+
+// isPositive(5);
+// isPositive(-3);
+// isPositive(0);
+// isPositive(10);
+// isPositive(-1);
+
+// function minTwoNumbers(a, b) {
+//     if(a > b) {
+//         console.log(b);
+        
+//     }else {
+//         console.log(a);
+        
+//     }
+// }
+
+// minTwoNumbers(3, 7);
+// minTwoNumbers(-1, 5);
+// minTwoNumbers(10, 10);
+// minTwoNumbers(-5, -10);
+// minTwoNumbers(2.5, 1.5);
+
+// function isPalindrome(str) {
+//     let str2 = str.split("").reverse().join("")
+
+//     if(str2 === str) {
+//         console.log(true);
+        
+//     }else {
+//         console.log(false);
+        
+//     }
+// }
+
+// isPalindrome("level");
+// isPalindrome("hello");
+// isPalindrome("madam");
+// isPalindrome("racecar");
+// isPalindrome("world");
+
+// function maxThreeNumbers(a, b, c) {
+//     if(a > b && a > c) {
+//         console.log(a);
+        
+//     }else if(b > a && b > c) {
+//         console.log(b);
+        
+//     }else {
+//         console.log(c);
+        
+//     }
+// }
+
+// maxThreeNumbers(3, 7, 5);
+// maxThreeNumbers(-1, 5, 2);
+// maxThreeNumbers(10, 10, 10);
+// maxThreeNumbers(-5, -10, -3);
+// maxThreeNumbers(2.5, 1.5, 3.5);
+
+// function multiplyTwoNumbers(a, b) {
+//     console.log(a * b);
+    
+// }
+
+// multiplyTwoNumbers(3, 7);
+// multiplyTwoNumbers(-1, 5);
+// multiplyTwoNumbers(0, 10);
+// multiplyTwoNumbers(2.5, 2);
+// multiplyTwoNumbers(-2, -3);
